7ad301a1438f38ba532aa2e2eecf321826c83b02
adeshingale3 <adeshingale600@gmail.com>
2025-08-11 22:18:37 +0530

    a

--- duolingo/src/App.jsx
+++ duolingo/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import { FadingBG, GithubBG, MaskedBG } from 'wavyjs'
 
 const App = () => {
   return (
     <div className='h-screen w-screen relative z-999 flex justify-center'>
+      <GithubBG />
       <Navbar/>
       <Hero />
+
     </div>
+   
   )
 }
 
--- duolingo/src/components/Hero.jsx
+++ duolingo/src/components/Hero.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
+import { MaskedBG } from 'wavyjs';
 
 const Hero = () => {
   return (
-    <div className="bg-[#EFEFEF] min-h-screen w-full flex flex-col items-center justify-center relative px-4 text-center">
-      <h1 className="font-bold text-black text-4xl sm:text-5xl md:text-6xl leading-tight">
+    <div className="min-h-screen w-full flex flex-col items-center justify-center relative px-4 text-center">
+      <h1 className="absolute font-bold text-black text-2xl sm:text-4xl md:text-5xl leading-tight top-40 z-10">
         Duolingo, Because <br />
         <span className="text-red-500">'small talk'</span> shouldn't be scary.
       </h1>
 
-      <h3 className="text-black text-base sm:text-lg mt-6">
+      <h3 className="absolute text-black text-base sm:text-sm mt-6 top-65">
         You have mastered every excuse except the language you wanted to learn.
       </h3>
 
-      <button className="mt-8 h-12 px-6 bg-green-500 text-black font-semibold cursor-pointer rounded-xl shadow-md hover:shadow-lg transition hover:scale-105 duration-300">
+      <button className="absolute z-10 top-80 h-12 px-6 bg-green-500 text-black font-semibold cursor-pointer rounded-xl shadow-md hover:shadow-lg transition hover:scale-105 duration-300">
         Start Your Journey
       </button>
 
-      <p className="mt-4 text-sm cursor-pointer font-semibold hover:text-green-400 duration-300">
+      <p className="absolute z-10 text-sm cursor-pointer top-95 font-semibold hover:text-green-400 duration-300">
         Already have an account?
       </p>
-      <img
-        src="/Duolingo.png"
+      <div className='absolute z-5 h-85 w-85 flex items-center top-98 mt-2 text-sm font-semibold'>
+        <img
+        src="/duolingo-logo.png"
         alt="Duolingo Mascot"
-        className="mt-10 w-52 sm:w-64 drop-shadow-xl"
+        // className="absolute scale-125 sm:w-64 drop-shadow-xl bottom-15 z-5"
       />
+      </div>
+      
       {/* INDIA */}
 <div className="h-20 w-50 z-40 flex hidden sm:block flex-col items-center justify-between absolute rotate-[5deg] gap-2 left-60 " style={{ bottom: "25rem" }}>
   <img src="/india.jpeg" className=" rounded-xl shadow-2xl shadow-orange-500 border-4 border-white" />
